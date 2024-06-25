55a08d5a9d3bf33fdc6db8893e2532a5bb20bfd9
Candace Ryan <ryan.candace26@gmail.com>
2024-06-25 00:05:03 -0500

    implemented successful get requests with API key

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,15 +1,29 @@
 import { useState, useEffect } from 'react';
 
 import { copy, linkIcon, loader, tick} from '../assets';
+import { useLazyGetSummaryQuery } from '../services/article';
 
 const Demo = () => {
-  const [article, setarticle] = useState({
+  const [article, setArticle] = useState({
     url: '',
     summary: '',
   });
 
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
   const handleSubmit = async (e) => {
-    alert('Submitted');
+    e.preventDefault();
+
+    const { data } = await getSummary({
+      articleUrl: article.url
+    });
+
+    if(data?.summary) {
+      const newArticle = { ...article, summary: data.summary};
+      setArticle(newArticle);
+
+      console.log(newArticle);
+    }
   }
 
 
@@ -30,7 +44,7 @@ const Demo = () => {
           type='url'
           placeholder='Enter a valid URL'
           value={article.url}
-          onChange={(e) => setarticle({ ...
+          onChange={(e) => setArticle({ ...
             article, url: e.target.value
           })}
           required
